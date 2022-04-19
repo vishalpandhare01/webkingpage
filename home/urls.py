12078c9f9fd3ecf18e1de3a65034f9704a168eda
vishalpandhare01 github <vishal5544@gmail.com>
2022-04-19 11:14:20 +0530

    initial commit

--- home/urls.py
+++ home/urls.py
@@ -4,9 +4,9 @@ from .import views
 
 urlpatterns = [
     path('', views.index,name='index'),
-    path('reg/', views.reg,name='reg'),
-    path('login/', views.login,name='login'),
-    path('home/', views.home,name='home'),
+    path('auth/reg', views.reg,name='reg'),
+    path('auth/login', views.login,name='login'),
+    path('home', views.home,name='home'),
     path('services', views.services,name='services'),
     path('contact', views.contact,name='contact'),
 ]
