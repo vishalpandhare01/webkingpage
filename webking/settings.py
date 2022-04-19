d0a0f282997d87ac934dae2dcd146e02d084ac09
vishalpandhare01 github <vishal5544@gmail.com>
2022-04-19 11:37:45 +0530

    initial commit

--- webking/settings.py
+++ webking/settings.py
@@ -38,14 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home.apps.HomeConfig',
-    'survey',
-
-   
+    'survey',   
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -85,8 +83,8 @@ DATABASES = {
         'NAME': 'survey',
         'USER':'postgres',
         'PASSWORD':'Vishal@4162',
-        'HOST':'localhost',
-
+        'HOST':'127.0.0.1',
+        'port': '5432'
     }
 }
 
@@ -133,3 +131,4 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL = 'Home'
