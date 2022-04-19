cdac98c8f07a038414b760f018a62b4e818eae90
vishalpandhare01 github <vishal5544@gmail.com>
2022-04-19 11:43:45 +0530

    initial commit

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from django.urls import path
 from .import views
 
+from django.views.static import serve
+from django.conf.urls.static import url
+from django.conf import settings
+
 urlpatterns = [
     path('', views.index,name='index'),
     path('auth/reg', views.reg,name='reg'),
@@ -9,4 +13,7 @@ urlpatterns = [
     path('home', views.home,name='home'),
     path('services', views.services,name='services'),
     path('contact', views.contact,name='contact'),
+
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT})
 ]
